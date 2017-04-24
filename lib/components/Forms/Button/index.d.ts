6c83b20902a9248c676c5135d797f44265d2fd8d
Martin Hrncir <martin.hrncir@reglendo.com>
2017-04-24 12:52:18 +0200

    prepublish

---
+++ lib/components/Forms/Button/index.d.ts
@@ -0,0 +1,31 @@
+/// <reference types="react" />
+import * as React from "react";
+import InputProps from "../default_props";
+import Icon from "../../Icon";
+export interface Props extends InputProps {
+    type?: "button" | "link" | "submit";
+    link?: string;
+    icon?: Icon | JSX.Element;
+    color?: "blue" | "gray" | "grey" | "green" | "red" | "nocolor";
+    size?: "small" | "tiny" | "";
+    state?: "disabled" | "";
+    onClick?: (event: any) => boolean;
+    labels?: {
+        main?: string | JSX.Element;
+        invalid?: string | JSX.Element;
+        title?: string;
+    };
+}
+export interface State {
+}
+declare class Button extends React.Component<Props, State> {
+    readonly name: string;
+    readonly form: string;
+    static defaultProps: Props;
+    renderInvalid(): JSX.Element;
+    renderButton(): JSX.Element;
+    renderLink(): JSX.Element;
+    renderSubmit(): JSX.Element;
+    render(): JSX.Element;
+}
+export default Button;
