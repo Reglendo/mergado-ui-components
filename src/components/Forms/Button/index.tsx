a11cc9b0f27a0ce9f3c4095d835c8a2613005f24
Martin Hrncir <martin.hrncir@reglendo.com>
2017-04-24 12:41:39 +0200

    component button

---
+++ src/components/Forms/Button/index.tsx
@@ -0,0 +1,112 @@
+import * as React  from "react"
+import InputProps from "../default_props"
+import {prefix} from "../../../config"
+import Icon from "../../Icon"
+
+export interface Props extends InputProps {
+    type?: "button" | "link" | "submit"
+    link?: string
+    icon?: Icon | JSX.Element
+    color?: "blue" | "gray" | "grey" | "green" | "red" | "nocolor"
+    size?: "small" | "tiny" | ""
+    state?: "disabled" | ""
+    onClick?: (event: any) => boolean
+    labels?: {
+        main?: string | JSX.Element
+        invalid?: string | JSX.Element
+        title?: string
+    }
+}
+
+export interface State {
+}
+
+class Button extends React.Component<Props, State> {
+
+    readonly name = prefix + "button";
+    readonly form = prefix + "form";
+
+    public static defaultProps: Props = {
+        type: "button",
+        onClick: (event) => {
+            return true;
+        },
+        icon: null,
+        color: "blue",
+        state: "",
+        size: "",
+        input: {
+            checked: false,
+            name: "",
+            onBlur: (value) => {
+            },
+            onChange: (value) => {
+            },
+            onDragStart: (value) => {
+            },
+            onDrop: (value) => {
+            },
+            onFocus: (value) => {
+            },
+            value: ""
+        },
+        meta: {
+            active: false,
+            asyncValidating: false,
+            autofilled: false,
+            dirty: false,
+            dispatch: Function,
+            error: "",
+            form: "",
+            invalid: false,
+            pristine: true,
+            submitting: false,
+            submitFailed: false,
+            touched: false,
+            valid: true,
+            visited: false,
+            warning: ""
+        },
+        labels: {
+            main: null,
+            invalid: "Invalid input",
+            title: ""
+        }
+    }
+
+    renderInvalid() {
+        return (
+            <div
+                className="form-validation-box">{this.props.meta.invalid && (this.props.meta.dirty || this.props.meta.touched) ? this.props.labels.invalid : ""}</div>
+        )
+    }
+
+    renderButton() {
+        const { input, labels, icon, onClick } = this.props
+        return (<button className={`${this.name}__item`} onClick={onClick} name={input.name} title={labels.title}>{icon}{labels.main}</button>)
+    }
+    renderLink() {
+        const { link, labels, icon, onClick } = this.props
+        return (<a href={link} className={`${this.name}__item`} onClick={onClick} title={labels.title}>{icon}{labels.main}</a>)
+    }
+    renderSubmit() {
+        const { meta, input, labels, onClick } = this.props
+        const inputId = `${meta.form}-${input.name}`
+        return (<input type="submit" className={`${this.name}__item`} value={`${labels.main}`} id={inputId} name={input.name} title={labels.title} onClick={onClick} />)
+    }
+
+    render() {
+        const { type,color,state, size } = this.props
+
+        return (
+            <div className={`${this.name} ${this.name}--${color} ${!this.props.labels.main?this.name+`--notext`:``} ${size?this.name+`--`+size:``} ${state?this.name+`--`+state:``} ${this.form}__group `} title={this.props.labels.title}>
+                {this.renderInvalid()}
+                {type == 'button' && this.renderButton()}
+                {type == 'link' && this.renderLink()}
+                {type == 'submit' && this.renderSubmit()}
+            </div>
+        )
+    }
+}
+
+export default Button
