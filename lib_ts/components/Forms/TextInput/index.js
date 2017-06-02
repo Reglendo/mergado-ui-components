a871cce982651ac792f54900ebe8226d29ca0c2f
Jenkins <ping@reglendo.com>
2017-06-02 11:18:21 +0000

    js+css build [ci skip]

--- lib_ts/components/Forms/TextInput/index.js
+++ lib_ts/components/Forms/TextInput/index.js
@@ -15,10 +15,14 @@ class TextInput extends React.Component {
         const addProps = Object.assign({}, this.props.addProps);
         delete addProps.addClass;
         const props = this.props;
+        const inputProps = this.props.input;
+        if (type === "file") {
+            delete inputProps.value;
+        }
         return (React.createElement(Input_1.Input, Object.assign({ name: this.name }, props),
             React.createElement(Input_1.InputError, Object.assign({}, props)),
             React.createElement(Input_1.InputLabel, { name: this.name }, this.props.labels.main),
-            React.createElement("input", Object.assign({ id: inputId, type: type, placeholder: this.props.labels.placeholder, ref: "input" }, this.props.input, addProps, { className: `${this.name}__input ${this.form}__input--text ${this.form}__input--${type}
+            React.createElement("input", Object.assign({ id: inputId, type: type, placeholder: this.props.labels.placeholder, ref: "input" }, this.props.input, inputProps, { className: `${this.name}__input ${this.form}__input--text ${this.form}__input--${type}
                                 ${meta.invalid && (meta.dirty || meta.touched) ? "invalid" : ""}
                                 ${this.props.addProps ? this.props.addProps.addClass : ""}` }))));
     }
