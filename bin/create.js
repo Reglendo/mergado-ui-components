1692526643dea4a82e3b753f7afa1b56641edb14
Martin Hrnčíř <martin.hrncir@reglendo.com>
2018-12-10 20:08:50 +0100

    added esm exports

---
+++ bin/create.js
@@ -0,0 +1,17 @@
+var fs = require('fs');
+var glob = require('glob');
+var path = require('path');
+var rootDir = path.join(__dirname, '../src/components');
+
+glob(rootDir + '/**/index.tsx', function(err, components) {
+
+    var modules = components.map(c => {
+        const name = path.basename(path.dirname(c))
+        const componentPath = path.dirname(c).replace(path.dirname(__dirname)+'/src','')
+        return `export { default as ${name} } from '.${componentPath}';`;
+    }).join('\n') + '\n';
+
+    console.log(modules);
+    fs.writeFileSync(path.join(__dirname, '../src/index.ts'), modules, 'utf-8');
+    console.log('Exports written to ' + path.join(__dirname, '../src/index.ts'));
+});
--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,49 @@
-// export {default as Button} from "./components/Forms/Button"
-// export {default as Checkbox} from "./components/Forms/Checkbox"
-// export {default as CheckboxContainer} from "./components/Forms/CheckboxContainer"
-
-// export {default as Autocomplete} from "./components/Forms/Autocomplete"
-// export {default as Radio} from "./components/Forms/Radio"
-// export {default as Range} from "./components/Forms/Range"
-// export {default as Select} from "./components/Forms/Select"
-// export {default as TextInput} from "./components/Forms/TextInput"
-// export {default as Textarea} from "./components/Forms/Textarea"
-
-// export {default as LittleStatus} from "./components/LittleStatus"
-// export {default as Paginator} from "./components/Paginator"
-// export {default as Placeholder} from "./components/Placeholder"
-
-// export {default as PopupHint} from "./components/PopupHint"
-// export {default as Spinner} from "./components/Spinner"
-// export {default as Toasts} from "./components/Toasts"
-// export {default as Toast} from "./components/Toast"
-// export {default as TopNav} from "./components/Nav"
-// export {default as NavLink} from "./components/NavLink"
-// export {default as WizardNav} from "./components/WizardNav"
-// export {default as WizardStep} from "./components/WizardStep"
-
-// export {default as DataTable} from "./components/Datagrid/DataTable"
-// export {default as Datagrid} from "./components/Datagrid/DataTable" // BC compatibility
-// export {default as DataRow} from "./components/Datagrid/DataRow"
-// export {default as DataCell} from "./components/Datagrid/DataCell"
-// export {default as DataHeader} from "./components/Datagrid/DataHeader"
-// export {default as DataBody} from "./components/Datagrid/DataBody"
-
-
-// export {default as Fieldset} from "./components/Layout/Fieldset"
-// export {default as Header} from "./components/Layout/Header"
-// export {default as Section} from "./components/Layout/Section"
+export { default as Examples } from './components/Animations/Examples';
+export { default as Pulse } from './components/Animations/Pulse';
+export { default as Rotate } from './components/Animations/Rotate';
+export { default as App } from './components/App';
+export { default as Autocomplete } from './components/Autocomplete';
+export { default as Button } from './components/Button';
+export { default as Carousel } from './components/Carousel';
+export { default as Checkbox } from './components/Checkbox';
+export { default as CheckboxContainer } from './components/CheckboxContainer';
+export { default as ColorPicker } from './components/ColorPicker';
+export { default as DataBody } from './components/DataBody';
+export { default as DataCell } from './components/DataCell';
+export { default as DataHeader } from './components/DataHeader';
+export { default as DataRow } from './components/DataRow';
+export { default as DataTable } from './components/DataTable';
+export { default as DatePicker } from './components/DatePicker';
+export { default as Div } from './components/Div';
+export { default as Field } from './components/Field';
+export { default as FieldError } from './components/FieldError';
+export { default as FieldLabel } from './components/FieldLabel';
+export { default as Fieldset } from './components/Fieldset';
+export { default as Grid } from './components/Grid';
+export { default as GridCell } from './components/GridCell';
+export { default as Header } from './components/Header';
+export { default as IconFinder } from './components/Icon/IconFinder';
+export { default as Icon } from './components/Icon';
+export { default as LittleStatus } from './components/LittleStatus';
+export { default as Nav } from './components/Nav';
+export { default as NavLink } from './components/NavLink';
+export { default as Paginator } from './components/Paginator';
+export { default as Placeholder } from './components/Placeholder';
+export { default as Bubble } from './components/PopupHint/Bubble';
+export { default as PopupHint } from './components/PopupHint';
+export { default as Radio } from './components/Radio';
+export { default as Range } from './components/Range';
+export { default as Section } from './components/Section';
+export { default as Select } from './components/Select';
+export { default as Span } from './components/Span';
+export { default as Spinner } from './components/Spinner';
+export { default as Textarea } from './components/Textarea';
+export { default as TextInput } from './components/TextInput';
+export { default as Theme } from './components/Theme';
+export { default as Toast } from './components/Toast';
+export { default as Toasts } from './components/Toasts';
+export { default as Toggler } from './components/Toggler';
+export { default as Ul } from './components/Ul';
+export { default as Widget } from './components/Widget';
+export { default as WizardNav } from './components/WizardNav';
+export { default as WizardStep } from './components/WizardStep';
